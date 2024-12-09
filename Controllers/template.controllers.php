95f0b452579f53c4f989c2c9bb385717bf657248
Anderson Gordillo <mauri201111@gmail.com>
2024-12-09 14:07:09 -0500

    Initial commit

---
+++ Controllers/template.controllers.php
@@ -0,0 +1,9 @@
+<?php
+
+    class ControllerTemplate{
+        public function controllerTemplate(){
+            include "Views/template.php";
+        }
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+    include "Controllers/template.controllers.php";
+
+    $template = new ControllerTemplate;  //Llamar la clase 
+    $template -> controllerTemplate(); // Llamar el metodo
+
+    
+
+?>
